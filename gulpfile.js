563a533b1096b6bb8659ea21096474c08f145ddc
Anton Mills <hello@antonmills.com>
2014-08-20 17:29:18 +1000

    revamp of the gulp script for dev.

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,18 @@ var filesize = require('gulp-filesize');
 var rename = require('gulp-rename');
 var gutil = require('gulp-util');
 var clean = require('gulp-rimraf');
+var connect = require('gulp-connect');
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
 
-// clean dist
-gulp.task('clean', function () {
+// clean dist folder
+gulp.task('clean-dist', function () {
     return gulp.src('./dist', {read: false})
         .pipe(clean());
 });
 
-gulp.task('build', function() {
+// main build script
+gulp.task('build-js', function() {
     return gulp.src('src/*.js')
         // concatenate and generate trail.js
         .pipe(concat('trail.js'))
@@ -25,8 +29,30 @@ gulp.task('build', function() {
         .pipe(uglify())
         .pipe(rename('trail.min.js'))
         .pipe(gulp.dest('dist'))
+
+        // create version in examples
+        .pipe(gulp.dest('./examples/js'))
         .pipe(filesize())
 
         // error logging
         .on('error', gutil.log)
 });
+
+// launch the examples server
+gulp.task('server-examples', function() {
+    connect.server({
+        port: 8080,
+        root: './examples'
+    });
+});
+
+// file watcher for auto-build and examples reload
+gulp.task('watch', function () {
+    gulp.watch('./src/*.*', ['reload']);
+});
+
+
+// default gulp task
+gulp.task('default', ['server-examples']);
+gulp.task('dev', ['clean-dist', 'build-js', 'watch']);
+gulp.task('build', ['clean-dist', 'build-js']);
